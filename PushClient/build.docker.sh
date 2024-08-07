8ec76f7f8b8628f02b6f9078901d2c33c0020337
George Legun <georgelegun@gmail.com>
2024-08-07 16:06:25 +0300

    Use git in dockerfile + build script

--- PushClient/build.docker.sh
+++ PushClient/build.docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+VERSION=$1
+if [ -z "$VERSION" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+echo "Building version $VERSION"
+
 set -e;
-docker build -f Containerfile -t localhost/push-client .;
+docker build -f Containerfile --build-arg VERSION="$VERSION" -t localhost/push-client .;
 docker compose up -d --force-recreate \
    && docker container logs --follow nexxiot-push-node
--- PushClient/build.podman.sh
+++ PushClient/build.podman.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+VERSION=$1
+if [ -z "$VERSION" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+echo "Building version $VERSION"
+
 set -e;
-podman build -f Containerfile -t localhost/push-client .;
+podman build -f Containerfile --build-arg VERSION="$VERSION" -t localhost/push-client .;
 podman-compose up -d --force-recreate \
    && podman container logs --follow nexxiot-push-node
